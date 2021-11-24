2132050707b19eb2d973692e6ff87ee879e0bab3
Shubham Mittal <shubham18101@iiitd.ac.in>
2021-11-25 01:53:35 +0530

    Fixed store in alloca to mymalloc

--- llvm/lib/CodeGen/SafeC/MemSafe.cpp
+++ llvm/lib/CodeGen/SafeC/MemSafe.cpp
@@ -69,6 +69,9 @@ bool isAllocaToMallocRequired(AllocaInst* AI, Function &F, const TargetLibraryIn
 	bool isPassedToRoutine = false;
 	bool isStoredInMemory = false;
 
+	std::set<Value*> descendants;
+	descendants.insert(AI);
+
 	while(not q.empty()){
 		
 		Instruction *InstPopped = q.front();
@@ -78,10 +81,10 @@ bool isAllocaToMallocRequired(AllocaInst* AI, Function &F, const TargetLibraryIn
 						
 			if (auto *CI = dyn_cast<CallInst>(U)) {
 				
-				if(isLibraryCall(dyn_cast<CallInst>(U), TLI))  // library calls are not analysed
+				if(isLibraryCall(CI, TLI))  // library calls are not analysed
 					continue;
 
-				isPassedToRoutine = true;
+				isPassedToRoutine = true;  // due to IR in SSA form, we need not to iterate over parametrs
 
 				if(DEBUG)
 					errs() << *CI << " breaking...";
@@ -93,6 +96,9 @@ bool isAllocaToMallocRequired(AllocaInst* AI, Function &F, const TargetLibraryIn
 			if(auto *SI = dyn_cast<StoreInst>(U)) {
 
 				if(not SI -> getValueOperand() -> getType() -> isPointerTy())
+					continue;
+
+				if(descendants.find(SI -> getValueOperand()) == descendants.end())
 					continue;
 
 				isStoredInMemory = true;
@@ -107,6 +113,7 @@ bool isAllocaToMallocRequired(AllocaInst* AI, Function &F, const TargetLibraryIn
 			if(auto *Inst = dyn_cast<Instruction>(U)) { 		// Fixme: What about branch instructions?
 				
 				q.push_back(Inst);
+				descendants.insert(Inst);
 				
 				if(DEBUG)
 					errs() << *Inst << "  pushed in deque\n";
@@ -115,7 +122,8 @@ bool isAllocaToMallocRequired(AllocaInst* AI, Function &F, const TargetLibraryIn
 	}
 
 	if(DEBUG)
-		errs() << "\nAlloca requires conversion?: " << ((isPassedToRoutine || isStoredInMemory)?"Yes":"No") << "\n\n\n";
+		errs() << "\nAlloca requires conversion?: " \
+			   << ((isPassedToRoutine || isStoredInMemory)?"Yes":"No") << "\n\n\n";
 
 	return isPassedToRoutine || isStoredInMemory;
 }
@@ -176,19 +184,28 @@ void convertAllocaToMyMalloc(Function &F, const TargetLibraryInfo *TLI){
 			size_t bytesAllocated = (*AI->getAllocationSizeInBits(DL)) / 8;
 
 			if(DEBUG)
-				errs() << "It is Not VLA Alloca, so size Allocated by Alloca: " << bytesAllocated << "\n";
+				errs() << "It is Non-VLA Alloca, so size Allocated by Alloca: " << bytesAllocated << "\n";
 
-			auto FnMalloc = F.getParent()->getOrInsertFunction("mymalloc", AI->getType(), Type::getInt64Ty(F.getContext())); // Fixeme: getType should be void* so bitcast is required from void* to alloca->getTpye
-			auto *FI = CallInst::Create(FnMalloc, ConstantInt::get(Type::getInt64Ty(F.getContext()), bytesAllocated, false));
+			auto FnMalloc = F.getParent()->getOrInsertFunction("mymalloc", AI->getType(), \
+							Type::getInt64Ty(F.getContext())); // Fixeme: getType should be void* so bitcast is required from void* to alloca->getTpye
+			
+			auto *FI = CallInst::Create(FnMalloc, ConstantInt::get(Type::getInt64Ty(F.getContext()), \
+													bytesAllocated, false));
 			ReplaceInstWithInst(AI, FI);
 			callInstructionsInsertedForNonVLA_Alloca.insert(FI);
 		}
 		else {														// it is VLA Alloca
 
 			auto sizeOfTypeAllocated = DL.getTypeAllocSize(AI->getAllocatedType());
-			Value *ObjSize = IRB.CreateMul( AI->getOperand(0), llvm::ConstantInt::get(llvm::Type::getInt64Ty(F.getContext()), sizeOfTypeAllocated));
-			auto FnMalloc = F.getParent()->getOrInsertFunction("mymalloc", AI->getType(), ObjSize -> getType());
+			
+			Value *ObjSize = IRB.CreateMul( AI->getOperand(0), \
+				llvm::ConstantInt::get(llvm::Type::getInt64Ty(F.getContext()), sizeOfTypeAllocated));
+			
+			auto FnMalloc = F.getParent()->getOrInsertFunction("mymalloc", \
+							AI->getType(), ObjSize -> getType());
+			
 			auto *FI = CallInst::Create(FnMalloc, ObjSize);
+			
 			ReplaceInstWithInst(AI, FI);
 			callInstructionsInsertedForVLA_Alloca.insert(FI);
 		}	
@@ -199,7 +216,8 @@ void convertAllocaToMyMalloc(Function &F, const TargetLibraryInfo *TLI){
 	 */
 	for(auto *callInstMalloc: callInstructionsInsertedForNonVLA_Alloca){
 		Instruction *lastInstruction = &(F.back().back());
-		auto FnFree = F.getParent()->getOrInsertFunction("myfree", Type::getVoidTy(F.getContext()), callInstMalloc->getType());
+		auto FnFree = F.getParent()->getOrInsertFunction("myfree", Type::getVoidTy(F.getContext()), \
+						callInstMalloc->getType());
 		IRBuilder<> IRB(lastInstruction);
 		IRB.CreateCall(FnFree, {callInstMalloc});
 	}
@@ -213,21 +231,128 @@ void convertAllocaToMyMalloc(Function &F, const TargetLibraryInfo *TLI){
 			CallInst *CI = dyn_cast<CallInst>(&I);
 			if(CI && CI->getCalledFunction() && CI->getCalledFunction()->getName() == "llvm.stackrestore"){
 				
-				for(auto callInstMalloc: callInstructionsInsertedForVLA_Alloca) {
-					auto FnFree = F.getParent()->getOrInsertFunction("myfree", Type::getVoidTy(F.getContext()), callInstMalloc->getType());
+				for(auto &callInstMalloc: callInstructionsInsertedForVLA_Alloca) {
+					auto FnFree = F.getParent()->getOrInsertFunction("myfree", \
+									Type::getVoidTy(F.getContext()), callInstMalloc->getType());
 					IRBuilder<> IRB(&I);
 					IRB.CreateCall(FnFree, {callInstMalloc});
 				}
-				
 				return;	
 			}
 		}
 	}
 }
 
+void insertCheckForOutOfBoundPointer(Function &F, const TargetLibraryInfo *TLI){
+	
+	std::set<std::pair<Value*, Value*>> pointersToTrack;
+
+	if(DEBUG){
+		errs() << "\n\n************************\nAfter myfree insertion" << "\n";
+		for (BasicBlock &BB : F) {
+			for (Instruction &I : BB) {
+				errs() << I << "\n";
+			}
+		}
+	}
+
+	for (BasicBlock &BB : F) {
+		for (Instruction &I : BB) {
+			
+			CallInst *CI = dyn_cast<CallInst>(&I);
+			if(CI && not isLibraryCall(CI, TLI) && CI->getCalledFunction()){
+				
+				if(CI->getCalledFunction()->getName() == "myfree" || CI->getCalledFunction()->getName() == "mymalloc")
+					continue;
+
+				for(auto arg = CI -> arg_begin() ; arg != CI -> arg_end() ; arg++){
+					
+					if(arg->get()->getType()->isPointerTy()){
+					
+						if(DEBUG) {
+							errs() << "\nCall Instruction with pointer passed: " << *CI << "\n";
+							errs() << "Track this pointer: " << *arg->get() << "\n";
+						}
+						pointersToTrack.insert({arg->get(), CI});
+					}
+				}
+			}
+
+			ReturnInst *RI = dyn_cast<ReturnInst>(&I);
+			if(RI && RI->getReturnValue() && RI->getReturnValue()->getType()->isPointerTy()){
+				
+				if(DEBUG) {
+					errs() << "\nReturn Instruction with pointer returned: " << *RI << "\n";
+					errs() << "Track this pointer: " << *RI->getReturnValue() << "\n";
+				}
+
+				pointersToTrack.insert({RI->getReturnValue(), RI});
+			}
+
+			StoreInst *SI = dyn_cast<StoreInst>(&I);
+			if(SI && SI->getOperand(0)->getType()->isPointerTy()){
+
+				if(DEBUG) {
+					errs() << "\nStore Instruction with pointer stored: " << *SI << "\n";
+					errs() << "Track this pointer: " << *SI->getOperand(1) << "\n";
+				}
+
+				pointersToTrack.insert({SI->getOperand(1), SI});
+
+			}
+		}
+	}
+
+	std::set<std::pair<Value*, std::pair<Value*, Value*>>> basePointers;
+	// now backtrack for each pointer
+	for(std::pair<Value*, Value*> ptr_Inst: pointersToTrack){
+		
+		auto ptr = ptr_Inst.first;
+
+		if(DEBUG)
+			errs() << "\n\nBackTracking: " << *ptr << "\n";
+		std::deque<Value*> q;
+		q.push_back(ptr);
+		while(not q.empty()){
+			auto *poppedInstr = q.front();
+			q.pop_front();
+			errs() << *poppedInstr << "\n";
+
+			for(auto &U: poppedInstr->uses()){
+				auto *Inst = dyn_cast<Instruction>(U);
+				if(Inst){
+					
+					if(auto *BI = dyn_cast<BitCastInst>(Inst)){
+						q.push_back(BI->getOperand(0));
+					}
+					else if(auto *GEPI = dyn_cast<GetElementPtrInst>(Inst)){
+						q.push_back(GEPI->getOperand(0));
+					}
+					else{
+						q.clear();
+						if(poppedInstr == ptr){
+							if (DEBUG)
+								errs() << "Not required check" << "\n";
+							break;
+						}
+						basePointers.insert({poppedInstr, ptr_Inst});
+					}
+				}
+			}
+		}
+	}
+	for(auto ptrs: basePointers){
+		auto basePtr = ptrs.first;
+		auto ptrOutOfBound = ptrs.second.first;
+		auto InstNeedCheck = ptrs.second.second;
+		errs() << "All 3 ready: " << *basePtr << " " << *ptrOutOfBound << " " << *InstNeedCheck << "\n";
+	}
+}
+
 bool MemSafe::runOnFunction(Function &F) {
 	TLI = &getAnalysis<TargetLibraryInfoWrapperPass>().getTLI();
 	convertAllocaToMyMalloc(F, TLI);
+	// insertCheckForOutOfBoundPointer(F, TLI);
   return true;
 }
 
